4da0422c5a71fd4fe5e4c4472414323c47b39247
r4 <r4@nobrain.org>
2022-05-29 20:46:25 +0200

    actually add number.h itself 🤦

---
+++ include/ds/number.h
@@ -0,0 +1,12 @@
+#ifndef __DS_NUMBER_H__
+#define __DS_NUMBER_H__
+
+/* Returns the smaller/larger value of x and y. */
+#define min(x, y) ((x) < (y) ? (x) : y)
+#define max(x, y) ((x) > (y) ? (x) : y)
+
+/* Tries to calculate x - y. If the result would be negative, returns 0 instead.
+ * Can safely be used on unsigned types like size_t. */
+#define sub_clamped(x, y) ((x) > (y) ? (x) - (y) : 0)
+
+#endif
